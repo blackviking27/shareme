f04ab22a0327048afaef96c4c2a5e5bfba106609
Abhishek <prince88258@gmail.com>
2023-02-13 22:11:42 +0530

    minor changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -71,6 +71,7 @@ const Sidebar = ({ user, closeToggle }) => {
             alt="user profile"
           />
           <p>{user.userName}</p>
+          <IoIosArrowForward />
         </Link>
       )}
     </div>
